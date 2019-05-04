211d794ca586dc000de2c77b53d762d98d817570
Dedê <47751527+srtadede@users.noreply.github.com>
2019-05-03 21:59:00 -0300

    Add files via upload

---
+++ aula02/index.php
@@ -0,0 +1,35 @@
+<?php
+
+$nome = "Dê Cellis";
+$idade = 25;
+
+echo $name;
+
+echo "Nome Completo: $nome Idade: $idade";
+
+$soma = 20 + 20;
+$subtracao = 20 - 20;
+$divisao = 20 / 20;
+$multiplicacao= 20 * 20;
+
+echo "Soma: ".$soma;
+
+$incremento = 1;
+echo ++$incremento;
+
+// OPERADORES RELACIONAIS
+// igual: ==
+// diferente: != ou <> 
+// menor: <
+// maior: >
+// menor ou igual: <=
+// maior ou igual: >=
+
+// OPERADORES LÓGICOS
+// e: && ou and
+// ou: or ou ||
+// xor
+// negacao: !
+
+
+?>
